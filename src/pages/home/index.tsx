38700321d676034029f9c5c3d3b3077f84755c31
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-12-11 23:09:11 -0300

    construindo tabela das moedas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,45 @@
+import { Link } from 'react-router-dom';
 import styles from './home.module.css';
-
+import { CiSearch } from 'react-icons/ci';
 
 export function Home(){
     return(
-        <div className={styles.container}>
-            <h1>Página home</h1>
-        </div>
+        <main className={styles.container}>
+            <form className={styles.form}>
+                <input type="text" placeholder='Digite o símbolo da moeda: BTC...'/>
+                <button type='submit'>
+                    <CiSearch size={30} color="#FFF"/>
+                </button>
+            </form>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope='col'>Moeda</th>
+                        <th scope='col'>Valor mercado</th>
+                        <th scope='col'>Preço</th>
+                        <th scope='col'>Volume</th>
+                    </tr>
+                </thead>
+                <tbody id='tbody'>
+                    <tr className={styles.tr}>
+                        <td className={styles.tdLabel}>
+                            <Link to="/detail/btc">
+                                <span className={styles.link}>Bitcoin</span> | BTC
+                            </Link>
+                        </td>
+                        <td className={styles.tdLabel}>
+                            R$ 19293
+                        </td>
+                        <td className={styles.tdLabel}>
+                            R$ 40.962
+                        </td>
+                        <td className={styles.tdProfit}>
+                            <span>-5.3</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
     )
 }
