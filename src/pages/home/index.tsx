3465c0d216f0a02ae6596bbf993d0a9aaab844c4
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-12-12 22:10:47 -0300

    utilizando a api e formatando

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './home.module.css';
 import { CiSearch } from 'react-icons/ci';
 
+interface CoinProps {
+    name: string,
+    delta_24h: string,
+    price: string,
+    symbol: string,
+    volume_24h: string,
+    market_cap: string,
+    formatedPrice: string,
+    formatedMarket: string
+}
+
+interface DataProps {
+    coins: CoinProps[];
+}
+
 export function Home(){
+    const [coins, setCoins] = useState<CoinProps[]>([]);
+    useEffect(() => {
+        function getData() {
+            fetch('https://sujeitoprogramador.com/api-cripto/?key=df0ad826b87a3771')
+            .then(response => response.json())
+            .then((data: DataProps) => {
+                let coinsData = data.coins.slice(0,15);
+
+                let price = Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+
+                const formatResult = coinsData.map((item) => {
+                    const formated = {
+                        ...item,
+                        formatedPrice: price.format(Number(item.price)),
+                        formatedMarket: price.format(Number(item.market_cap))
+                    }
+                    return formated;
+                })
+
+                setCoins(formatResult);
+            })
+        }
+        getData();
+    },[])
+
     return(
         <main className={styles.container}>
             <form className={styles.form}>
